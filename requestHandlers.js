173e5c72894f4cb3dea2b2c6b358e042c2e7cef4
vsuskla <klaus.torhaugen@gmail.com>
2014-04-02 18:20:25 +0200

    non blocking

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,17 +1,25 @@
 var exec = require("child_process").exec;
 
-function start(){
+function start(response){
 	console.log("Request handler 'start' was called.");
 	
     var content = "empty";
     exec("ls -lah", function (error, stdout, stderr) {
-	    content = stdout;
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write(stdout);
+    response.end; 
     });
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("Klaus sin server: " + content);
+	
 	return content;
 }
 function upload(){
 	console.log("Request handler 'upload' was called."); 
-	return "upload siden!";
+
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("upload siden!");
+    response.end;
 }
 
 exports.start=start;
--- server.js
+++ server.js
@@ -4,12 +4,10 @@ var url=require("url");
 function start(route, handle){
 function onRequest(request, response) {
 	var pathname = url.parse(request.url).pathname;
-	console.log("req: " + request.url);
+
 	console.log("Request for " + pathname + " received.");
-	var content = route(handle, pathname);
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Klaus sin server: " + content);
-	response.end();
+	route(handle, pathname, response);
+
 }
 http.createServer(onRequest).listen(8888);
 console.log("Server has started.");
