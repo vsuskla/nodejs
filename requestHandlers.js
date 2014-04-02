653aca6ceb016ef329e699b7e9e73e6599ae0680
vsuskla <klaus.torhaugen@gmail.com>
2014-04-02 18:34:37 +0200

    allmost working with object processing

--- requestHandlers.js
+++ requestHandlers.js
@@ -3,18 +3,14 @@ var exec = require("child_process").exec;
 function start(response){
 	console.log("Request handler 'start' was called.");
 	
-    var content = "empty";
     exec("ls -lah", function (error, stdout, stderr) {
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write(stdout);
-    response.end; 
+	   response.writeHead(200, {"Content-Type": "text/plain"});
+	   response.write(stdout);
+       response.end; 
     });
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Klaus sin server: " + content);
 	
-	return content;
 }
-function upload(){
+function upload(response){
 	console.log("Request handler 'upload' was called."); 
 
 	response.writeHead(200, {"Content-Type": "text/plain"});
