44fdc4c993cda975091766abdef3ac2927534104
Klaus Torhaugen <klaus.torhaugen@gmail.com>
2014-04-02 07:55:18 +0200

    returning somthing from the request

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ var url=require("url");
 
 function start(route, handle){
 function onRequest(request, response) {
-var pathname = url.parse(request.url).pathname;
-console.log("req: " + request.url);
-console.log("Request for " + pathname + " received.");
-route(handle, pathname);
-response.writeHead(200, {"Content-Type": "text/plain"});
-response.write("Hello World, " + pathname);
-response.end();
+	var pathname = url.parse(request.url).pathname;
+	console.log("req: " + request.url);
+	console.log("Request for " + pathname + " received.");
+	var content = route(handle, pathname);
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("Hello World, " + content);
+	response.end();
 }
 http.createServer(onRequest).listen(8888);
 console.log("Server has started.");
