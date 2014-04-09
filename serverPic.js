60e77818789b47996126fc61b8d625ee6b54557f
Klaus Torhaugen <klaus.torhaugen@gmail.com>
2014-04-09 06:45:03 +0200

    not working yet

--- serverPic.js
+++ serverPic.js
@@ -1,18 +1,13 @@
-var formidable=("formidable"),
-		http=require("http"),
-		url=require("url");
+var http=require("http");
+var url=require("url");
 
-http.createServer(function(req, res){
-	if (req.url == "/upload" && req.method.toLowerCase() == "post") {
-		var form = new formidable.IncomingForm();
-		form.parse(req, function(error, fields, files)) {
-			res.writeHead(200, {"content-type":"text/plain"});
-			res.write("recived upload:\n\n");
-			res.end(sys.inspect({fields: fields, files: files}))
-		});
-		return;
+function start(route,handle){
+	function onRequest(request, response) {
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for " + pathname + " received.");
+		route(handle, pathname, response, request);
 	}
-	
-})
-
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started.");
+}
 exports.start=start;
